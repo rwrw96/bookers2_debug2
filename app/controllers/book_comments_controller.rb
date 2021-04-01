08c5f770f118a4bbd57fb944d8160d6147bf602f
EC2 Default User <ec2-user@ip-172-31-27-181.ec2.internal>
2021-04-01 12:58:33 +0000

    コメント機能途中。送信ボタン反応せず。調べる必要あり。

---
+++ app/controllers/book_comments_controller.rb
@@ -0,0 +1,20 @@
+class BookCommentsController < ApplicationController
+  def create
+    book = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments.build(book_comment_params)
+    @book_comment.book_id = book_id
+    @book_comment.save
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    @book_comment = Book_comment.where(book_id: params[:book_id], user_id: current_user.id)
+    @book_comment.destroy
+    redirect_to book_path(book_comment.book)
+  end
+  
+  private
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
+  get 'book_comments/create'
+  get 'book_comments/destroy'
   get 'favorites/create'
   get 'favorites/destroy'
   root 'homes#top'
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
   resources :books do
-    resource :favorites
-  end   
+    resource :favorites, only: [:create, :destroy]
+    resource :book_comments, only: [:create, :destroy]
+  end 
+  
   
   get 'home/about' => "homes#about"
   
