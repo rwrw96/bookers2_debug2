2ce0c606216323399eaab04d113958e4b9756b8f
EC2 Default User <ec2-user@ip-172-31-25-92.ec2.internal>
2021-04-02 10:25:42 +0000

    follow機能実装途中。リレーションに問題あり

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,19 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites
   has_many :book_comments, dependent: :destroy
+  
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :follower_id, dependent: :destroy
+  has_many :followers, through: :active_relationships, source: :follower
+  
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :followed_id, dependent: :destroy
+  has_many :followeds, through: :passive_relationships, source: :followed
+  
   attachment :profile_image, destroy: false
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50}
+  
+#   def followed_by?(user)
+#     active_relationships.find_by(followed_id: user.id).present?
+#   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+  get 'relationships/create'
+  get 'relationships/destroy'
   get 'book_comments/create'
   get 'book_comments/destroy'
   get 'favorites/create'
   get 'favorites/destroy'
   root 'homes#top'
   devise_for :users
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+  end
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resource :book_comments, only: [:create, :destroy]
