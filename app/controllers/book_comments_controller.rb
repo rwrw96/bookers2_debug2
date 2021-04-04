2cded7c69786eecc5334e06b55255b3a3ff42c64
rwrw96 <kazuki137@gmail.com>
2021-04-04 03:17:36 +0000

    book_comments controllerの修正

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,14 +5,12 @@ class BookCommentsController < ApplicationController
     @book_comment = @book.book_comments.build(book_comment_params)
     @book_comment.user_id = current_user.id
     @book_comment.save
-    @book_comments = @book.book_comments
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     @book_comment = BookComment.find_by(book_id: params[:book_id], user_id: current_user.id)
     @book_comment.destroy
-    @book_comments = @book.book_comments
   end
   
   private
