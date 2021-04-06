5b2b35925600ff5536f361d57e73d04f709be119
rwrw96 <kazuki137@gmail.com>
2021-04-06 00:37:25 +0000

    chat機能非同期通信修正、検索機能修正

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -2,7 +2,6 @@ class ChatsController < ApplicationController
   def create
     if UserRoom.where(user_id: current_user.id).present?
       @chat = Chat.create(params.require(:chat).permit(:user_id, :room_id, :content).merge(user_id: current_user.id))
-      redirect_back(fallback_location: root_path)
     else
       flash[:alert] = "メッセージ送信に失敗しました。"
       redirect_to room_path(@chat.room_id)
